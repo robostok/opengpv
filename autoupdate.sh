f7a41f4e70a109943a271388f31157c15dacea80
Robostok <robostoksolutions@gmail.com>
2021-10-19 15:50:51 +0100

    Added auto update

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-cd /home/pi/greenpass
-git pull
-pip3 install -r requirements.txt
-sudo service greenpassverifier restart
+
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
+EXP_VER=$(curl -s 'https://raw.githubusercontent.com/robostok/opengpv/main/update.json' | jq -r '.version')
+
+CUR_VER=$(cat ./update.json| jq -r '.version')
+
+echo $EXP_VER
+echo $CUR_VER
+
+if version_gt "$EXP_VER" "$CUR_VER"; then
+    git pull
+    pip3 install -r requirements.txt
+    sudo service opengpv restart
+else
+    echo 'No Update Required'
+fi
+
